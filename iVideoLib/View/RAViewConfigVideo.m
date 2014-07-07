68788fd11f7e57da64de2ded7647eb1b6df8f1c9
Philippe Fouquet <p.fouquet@environnement-sa.com>
2014-07-07 17:16:35 +0200

    Fist work for drag and drop

--- iVideoLib/View/RAViewConfigVideo.m
+++ iVideoLib/View/RAViewConfigVideo.m
@@ -235,10 +235,43 @@ NSImage* cgImageToNSImage(CGImageRef image)
     }
 }
 
+- (BOOL)outlineView:(NSOutlineView *)outlineView acceptDrop:(id < NSDraggingInfo >)info item:(id)item childIndex:(NSInteger)index {
+    //NSLog(@"accepting drag operation");
+    //todo: move the object in the data model;
+    NSIndexPath *path = [_ChapitreTree selectionIndexPath]; // these three values are nil too.
+    NSArray *objects = [_ChapitreTree selectedObjects];
+    NSArray *nodes = [_ChapitreTree selectedNodes];
+    //NSLog(@"%@", path);
+    //NSLog(@"%@", objects);
+    //NSLog(@"%@", nodes);
+    return YES;
+}
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView writeItems:(NSArray *)items toPasteboard:(NSPasteboard *)pboard {
+    NSString *pasteBoardType = @"Event";
+    [pboard declareTypes:[NSArray arrayWithObject:pasteBoardType] owner:self];
+    
+    return YES;
+}
+
+- (NSDragOperation)outlineView:(NSOutlineView *)outlineView
+                  validateDrop:(id < NSDraggingInfo >)info
+                  proposedItem:(id)item
+            proposedChildIndex:(NSInteger)index {
+    return NSDragOperationGeneric;
+}
+
+// This method gets called by the framework but the values from bindings are used instead
+- (id)outlineView:(NSOutlineView *)outlineView objectValueForTableColumn:(NSTableColumn *)tableColumn byItem:(id)item {
+    return NULL;
+}
+
 - (void)reloadData {
+    [_Outline registerForDraggedTypes:[NSArray arrayWithObject: @"Event"]];
     NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"position" ascending:YES];
     [_ChapitreTree setSortDescriptors:[NSArray arrayWithObject:sort]];
     [self setUrlToPlayer];
+    [_Outline reloadData];
 }
 
 
